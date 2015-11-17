c5808833745e2aeac9aaf034e1ffdf07d77e98df
jessieli93 <jli83@VDI-CNT2015-129.nait.ca>
2015-11-17 15:47:13 -0700

    ICA6
    recursion school work

---
+++ CMPE1700_JingLi_ICA6/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CMPE1700_JingLi_ICA6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> l = new List<int>();
+            l.Add(65);
+            l.Add(51);
+            l.Add(32);
+            l.Add(9);
+            l.Add(38);
+            l.Add(329);
+            int sum = 0;
+            int s = sum_re(l.Count - 1, sum, l);
+            Console.WriteLine(s);
+            Console.ReadKey();
+        }
+        static int sum_re(int count, int sum, List<int> l)
+        {
+            if(l == null)
+            {
+                return 0;
+            }
+            else if (count == 0)
+            {
+                sum += l[count];
+                return sum;
+            }
+            else
+            {
+                sum += l[count];
+                return sum_re(count -= 1, sum, l);
+            }
+        }
+    }
+}
